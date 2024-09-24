7a63152e476a4f5f49d1efb57f4072e83b864363
Andres Gomez <andresgouts@gmail.com>
2024-09-24 11:57:35 -0500

    Initial commit

---
+++ src/main/java/com/lepe/parking_check_in_mongo/ParkingCheckInMongoApplication.java
@@ -0,0 +1,13 @@
+package com.lepe.parking_check_in_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingCheckInMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingCheckInMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lepe/parking_check_in_mongo/ParkingCheckInMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lepe.parking_check_in_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingCheckInMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
